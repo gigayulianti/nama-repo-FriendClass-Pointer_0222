1fd1a72caf470cf806f5cf76993090a249d4387e
gigayulianti <yulianti2@gmail.com>
2026-06-14 01:30:35 +0700

    commit 14

--- pointerDua.cpp
+++ pointerDua.cpp
@@ -9,3 +9,13 @@ public:
     {
         cout << "No Induk = " << nim << endl;
     }   
+};
+
+int main()
+{
+    mahasiswa *mhs = new mahasiswa{1};
+    mhs->nim = 2;
+    mhs->showNim();
+    delete mhs;
+    return 0;
+}
